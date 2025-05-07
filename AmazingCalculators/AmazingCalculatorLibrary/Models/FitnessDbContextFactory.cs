78bc327630011cdc0dd943d3d8dfef1614addb67
JustJay <140190051+JustJaysRepo@users.noreply.github.com>
2025-05-07 12:49:07 -0400

    Add FitnessDbContextFactory for design-time support
    Implemented `FitnessDbContextFactory` to facilitate the creation of `FitnessDbContext` instances at design time. This class uses SQL Server as the database provider with a specified connection string.

---
+++ AmazingCalculators/AmazingCalculatorLibrary/Models/FitnessDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace AmazingCalculatorLibrary.Models
+{
+    public class FitnessDbContextFactory : IDesignTimeDbContextFactory<FitnessDbContext>
+    {
+        public FitnessDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<FitnessDbContext>();
+            optionsBuilder.UseSqlServer("Server=tcp:ccad18.database.windows.net,1433;Initial Catalog=fitnessdb;Persist Security Info=False;User ID=c3repo;Password=Pa$$w0rd!!!!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+
+            return new FitnessDbContext(optionsBuilder.Options);
+        }
+    }
+}
