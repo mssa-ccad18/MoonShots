512aa8675a0ab3edc5c559206a82f0dfb9c9e3c4
lmoran291 <26226799+lmoran291@users.noreply.github.com>
2025-05-07 12:35:15 -0400

    pushing bmi updates to my local branch

--- AmazingCalculators/AmazingCalculatorLibrary/Models/BMI.cs
+++ AmazingCalculators/AmazingCalculatorLibrary/Models/BMI.cs
@@ -6,8 +6,53 @@ using System.Threading.Tasks;
 
 namespace AmazingCalculatorLibrary.Models
 {
-    internal class BMI
+    public class BMI
     {
-        //test
+
+        public double WeightLbs { get; set; }
+        public double HeightFeet { get; set; }
+        public double HeightInches { get; set; }
+
+        public BMI(double weightLbs, double heightFeet, double heightInches)
+        {
+            WeightLbs = weightLbs;
+            HeightFeet = heightFeet;
+            HeightInches = heightInches;
+        }
+
+        private double ConvertHeightToInches()
+        {
+            return (HeightFeet * 12) + HeightInches;
+        }
+
+        public double Calculate()
+        {
+            double heightInTotalInches = ConvertHeightToInches();
+            if (heightInTotalInches <= 0)
+                throw new ArgumentException("Height must be greater than zero.");
+
+            return (WeightLbs * 703) / (heightInTotalInches * heightInTotalInches);
+        }
+
+        public string GetBMICategory()
+        {
+            double bmi = Calculate();
+
+            if (bmi < 18.5)
+                return "UnderWeight";
+            else if (bmi >= 18.5 && bmi <= 24.9)
+                return "Normal weight";
+            else if (bmi >= 25.0 && bmi <= 29.9)
+                return "Overweight";
+            return "obese";
+
+        }
+        public double TestConvertHeightToInches()
+        {
+            return ConvertHeightToInches();
+        }
     }
+
+
 }
+
---
+++ AmazingCalculators/AmazingCalculatorsTest/BMITest.cs
@@ -0,0 +1,29 @@
+using AmazingCalculatorLibrary.Models;
+
+namespace AmazingCalculatorsTest;
+
+[TestClass]
+public class BMITest
+
+{
+    [TestMethod]
+ 
+    public void ConvertHeightToInches_ShouldReturn63_WhenHeightIs5Feet3Inches()
+    {
+        // Arrange
+        double weight = 150; // Weight is irrelevant for this test
+        double heightFeet = 5;
+        double heightInches = 3;
+        BMI bmi = new BMI(weight, heightFeet, heightInches);
+
+        // Act
+        var result = bmi.TestConvertHeightToInches(); 
+
+        // Assert
+        Assert.AreEqual(63.0, result);
+    }
+    
+
+
+
+}
