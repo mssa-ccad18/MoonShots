dc0b533cc3f07876955b227a8497d4eb45ac3b1e
Kyle Giffitts <204399760+Kyle-Griffitts@users.noreply.github.com>
2025-05-09 18:27:42 -0400

    Adding a json file and adding data to USMC calc

--- AmazingCalculators/AmazingCalculatorLibrary/MilitaryPhysicalTraining/USMC.cs
+++ AmazingCalculators/AmazingCalculatorLibrary/MilitaryPhysicalTraining/USMC.cs
@@ -31,35 +31,83 @@ namespace AmazingCalculatorLibrary.MilitaryPhysicalTraining
                 ? DateTime.Now.Year - userProfile.DateOfBirth.Value.Year
                 : 0;
 
+            var isMale = userProfile.IsMale;
+            var isFemale = !isMale;
+
         }
 
         protected int _pullUps;
         protected int _crunches;
         protected int _runTime;
         protected int _pushups;
-        public void USMCMalePRT(bool IsMale, int age)
+        public void USMCMalePRT(bool isMale, int age)
         {
             if (age < 17)
             {
-
-
-
+                Console.WriteLine("Table values do not exist for ages under 17.");
+                return;
             }
             else if (age >= 17 && age < 21)
             {
+            
+                
+            }
+            else if (age >= 21 && age < 26)
+            {
 
+            }
+            else if (age >= 26 && age < 31) 
+            {
+            }
+            else if (age >= 31 && age < 36)
+            {
+            }
+            else if (age >= 36 && age < 41)
+            {
+            }
+            else if (age >= 41 && age < 46)
+            {
+            }
+            else if (age >= 46 && age < 51)
+            {
+            }
+            else if (age >= 51)
+            {
+            }
+        }
+        public void USMCFemalePRT(bool isFemale, int age)
+        {
+            if (age < 17)
+            {
+                Console.WriteLine("Table values do not exist for ages under 17.");
+                return;
+            }
+            else if (age >= 17 && age < 21)
+            {
 
             }
             else if (age >= 21 && age < 26)
             {
 
             }
-            else if (age >= 26 && age < 30)
-            { 
-            
-
+            else if (age >= 26 && age < 31)
+            {
             }
-
+            else if (age >= 31 && age < 36)
+            {
+            }
+            else if (age >= 36 && age < 41)
+            {
+            }
+            else if (age >= 41 && age < 46)
+            {
+            }
+            else if (age >= 46 && age < 51)
+            {
+            }
+            else if (age >= 51)
+            {
+            }
 
         }
 
